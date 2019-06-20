f0b98198f0ff0b6a648439d43b171daeacc5fc68
huangjiankang <huangjiankang@yunlizhihui.com>
2019-06-20 16:06:24 +0800

    添加创建项目后自动执行 npm install 指令

--- bin/index.js
+++ bin/index.js
@@ -3,26 +3,24 @@ const program = require('commander');
 const chalk = require('chalk');
 const fs = require('fs-extra');
 const path = require('path');
-const download = require('download-git-repo');
-const ora = require('ora');
 const inquirer = require('inquirer');
-
+const downloadTemplate = require('../lib/down')
 const logo =
-  `                                                                                                                                  
+  `
+  ----------------------------------------------------------------------
+  
+  KK  KK  OOOOO    AAA    CCCCC  RRRRRR  EEEEEEE   AAA   TTTTTTT EEEEEEE 
+  KK KK  OO   OO  AAAAA  CC    C RR   RR EE       AAAAA    TTT   EE      
+  KKKK   OO   OO AA   AA CC      RRRRRR  EEEEE   AA   AA   TTT   EEEEE   
+  KK KK  OO   OO AAAAAAA CC    C RR  RR  EE      AAAAAAA   TTT   EE      
+  KK  KK  OOOO0  AA   AA  CCCCC  RR   RR EEEEEEE AA   AA   TTT   EEEEEEE 
 
-88  dP  dP"Yb     db             8b    d8    db    88  dP 888888 88""Yb 
-88odP  dP   Yb   dPYb   ________ 88b  d88   dPYb   88odP  88__   88__dP 
-88"Yb  Yb   dP  dP__Yb  """""""" 88YbdP88  dP__Yb  88"Yb  88""   88"Yb  
-88  Yb  YbodP  dP""""Yb          88 YY 88 dP""""Yb 88  Yb 888888 88  Yb 
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+  ----------------------------------------------------------------------                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
 `
-
-
 console.log(chalk.cyan(logo));
-
 program
   .command('init')
-  .option('--name', '文件夹名称')
+  .option('--name', '< project name >')
   .action(function (cmd, option) {
     if (typeof cmd === 'string') {
       let proejctName = path.join(process.cwd(), cmd)
@@ -30,26 +28,28 @@ program
         if (exists) {
           inquirer.prompt([{
             type: 'confirm',
-            message: '文件名已存在是否替换',
+            message: 'file has already exist,replaced ?',
             name: 'ok'
           }]).then(answers => {
             if (answers.ok) {
               fs.emptyDir(proejctName, err => {
                 if (err) {
-                  console.log(err)
+                  console.log(err);
+                  process.exit(1);
                 } else {
                   downloadTemplate(proejctName, cmd)
                 }
               })
 
             } else {
-              //console.log('不继续')
+              process.exit(0);
             }
           })
         } else {
           fs.ensureDir(proejctName, err => {
             if (err) {
               console.log(err);
+              process.exit(1);
             } else {
               downloadTemplate(proejctName, cmd)
             }
@@ -57,27 +57,9 @@ program
         }
       })
     } else {
-      console.log('缺少文件名称')
+      console.log('please input your project name and run like this "koa-create init koa-project"');
+      process.exit(1);
     }
   })
-function downloadTemplate(proejctName, cmd) {
-  const spinner = ora('downloading template')
-  spinner.start();
-  download('jkhuangfu/koa-template', proejctName, function (err) {
-    spinner.stop();
-    console.log(err ? err
-      : chalk.green(
-        `
-        项目生成成功,执行以下命令进行启动: 
-        cd ${cmd} 
-        npm install 
-        npm start
-        `
-      )
-    )
-  })
-}
 
 program.parse(process.argv)
-
-
